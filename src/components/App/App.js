b9833fd4e96c43ffdf05a8176ed3e47ce9e504f6
clavatar <calavatar@yandex.ru>
2022-09-11 19:02:12 +0300

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,10 +17,10 @@ import InfoToolTip from '../InfoTooltip/InfoTooltip';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import Api from '../../utils/MainApi';
-import MoviesApi from '../../utils/MoviesApi';
 import * as Auth from '../../utils/Auth';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import Navigation from "../Navigation/Navigation";
+import useMoviesLogic from '../../utils/FilteringLogicHooks';
 
 
 function App(props) {
@@ -29,12 +29,8 @@ function App(props) {
     const [isInfoTooltipOpen, setIsInfoTooltipOpen] = React.useState(false);
     const [isNavigationOpen, setIsNavigationOpen] = React.useState(false);
     const [currentUser, setCurrentUser] = React.useState(null);
-    const [moviesList, setMoviesList] = React.useState(undefined);
-    const [savedMoviesList, setSavedMoviesList] = React.useState(undefined);
-    const [savedMoviesFiltered, setSavedMoviesFiltered] = React.useState(undefined);
     const [tooltipMessage, setTooltipMessage] = React.useState('');
     const [tooltipIsOk, setTooltipIsOk] = React.useState(false);
-    const [loading, setLoading] = React.useState(false);
     
     const handleInfoTolltipOpen = (tooltipMessage, tooltipIsOk) => {
         setTooltipIsOk(tooltipIsOk);
@@ -68,111 +64,22 @@ function App(props) {
         [setCurrentUser] 
     );
 
+    // подключаем логику работы с фильмами
+    const {
+        moviesList, 
+        savedMoviesFiltered, 
+        loading, 
+        moviesSearchHandler, 
+        savedMoviesFilterHandler,
+        loadSavedMovies, 
+        favoriteHandler, 
+        removeSavedMovieHandler,
+        clearAllMovies
+    } = useMoviesLogic (Api);
+
     const closeAllPopups = () => {
         setIsInfoTooltipOpen(false);
         setIsNavigationOpen(false);
-    }
-
-    function moviesSearchHandler(searchText, shorts) {
-        setLoading(true);
-        // запрашиваем все вильмы и все сохраненные одними промисом
-        Promise.all([MoviesApi.getMovies(), Api.getSavedMovies()])
-        .then(([movies, savedMovies])=>{
-            // фильтруем массив полученных фильмов
-            const filtered = filterMovies(movies, searchText, shorts);
-            // добавляем свойства наличия в сохраненных и исправляем ссылки
-            filtered.forEach(item => {
-                // ищем в сохраненных и добавляем свойство savedMovieId если фильм найден  
-                const sm = savedMovies.find(({movieId})=>movieId===item.id);
-                item.savedMovieId=sm ? sm._id : null;
-                // исправляем сcылки
-                item.imageLink=MoviesApi.baseUrl() + '/' + item.image.url
-                item.thumbnailLink=MoviesApi.baseUrl() + '/' + item.image.formats.thumbnail.url;
-            });
-            setLoading(false);
-            setMoviesList(filtered);
-        })
-        .catch(err=>{
-            setLoading(false);
-            console.log(err);
-        });
-    }
-
-    function loadSavedMovies() {
-        // устанавливаем признак загрузки данных
-        setLoading(true);
-        Api.getSavedMovies()
-        .then((res)=>{
-            // добавляем свойства для унификации с карточками фильмов
-            res.forEach(item=>{
-                // приводим элемент массива к единому виду
-                item.id=item._id
-                item.savedMovieId=item._id;
-                item.imageLink=item.image;
-                item.thumbnailLink=item.thumbnail;
-            })
-            // снимаем признак загрузки данных
-            setLoading(false);
-            setSavedMoviesFiltered(res);
-            setSavedMoviesList(res);
-        })
-        .catch(err=>{
-            // снимаем признак загрузки данных
-            setLoading(false);
-            console.log(err);
-        });
-    }    
-    
-    function savedMoviesFilterHandler(searchText, shorts) {
-        setSavedMoviesFiltered(filterMovies(savedMoviesList,searchText,shorts));
-    }
-
-    function filterMovies(data, searchText, shorts) {
-        const regEx = new RegExp(searchText,'i')
-        return data.filter(item=>regEx.test(item.nameRU) && item.duration<=(shorts ? 40 : 9999999))
-    }
-
-    function favoriteHandler(movie) {
-        // приводим карточку фильма к виду, пригодному для сохранения
-        const mv = {
-            // country: movie.country ? movie.country : '  ',
-            country: movie.country,
-            director: movie.director,
-            duration: movie.duration,
-            year: movie.year,
-            description: movie.description,
-            image: movie.imageLink,
-            trailerLink: movie.trailerLink,
-            thumbnail: movie.thumbnailLink,
-            movieId: movie.id,
-            nameRU: movie.nameRU,
-            nameEN: movie.nameEN
-        }
-
-        // в зависимости от наличия сохраненнонго фильма используем либо функцию добавления либо удаления, не теряя контекст
-        const func = movie.savedMovieId ? Api.deleteMovie.bind(Api) : Api.saveMovie.bind(Api);
-        
-        // вызываем функцию с параметром новой карточки или id карточки, которую надо удалить
-        func(movie.savedMovieId ? movie.savedMovieId : mv)
-        .then((res)=>{
-            movie.savedMovieId=movie.savedMovieId ? null : res._id;
-            setMoviesList(prev=>{return prev.map(c=>c.id===movie.id ? movie : c)});
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-
-    } 
-
-    function removeSavedMovieHandler(movie) {
-        Api.deleteMovie(movie._id)
-        .then(res=>{
-            if (savedMoviesList && savedMoviesList.length>0) 
-                setSavedMoviesList(savedMoviesList.filter(item=>item._id!==movie._id));
-            if (savedMoviesFiltered && savedMoviesFiltered.length>0)
-                setSavedMoviesFiltered(savedMoviesFiltered.filter(item=>item._id!==movie._id));
-        })
-        .catch(err=>console.log(err))
     }
 
     function handleRegister (name, email, password) {
@@ -216,7 +123,7 @@ function App(props) {
     }
     
     function handleSignOut() {
-        setMoviesList([]);
+        clearAllMovies();
         localStorage.removeItem('token');
         setCurrentUser(null);
         navigate('/');
@@ -237,7 +144,7 @@ function App(props) {
             } else {
                 handleInfoTolltipOpen('Что-то пошло. \n Попробуйте еще раз', false);
             }
-    })
+        })
     }
 
     return (
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,22 +2,25 @@ import { Link } from 'react-router-dom';
 import './Register.css';
 import logo from '../../images/logo.svg'
 import React from 'react';
+import { useFormWithValidation } from '../../utils/FormHooks';
 
 function Register(props) {
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
+    // const [name, setName] = React.useState('');
+    // const [email, setEmail] = React.useState('');
+    // const [password, setPassword] = React.useState('');
 
-    function handleNameChange(e) {setName(e.target.value)};
-    function handleEmailChange(e) {setEmail(e.target.value)};
-    function handlePasswordChange(e) {setPassword(e.target.value)};
+    // function handleNameChange(e) {setName(e.target.value)};
+    // function handleEmailChange(e) {setEmail(e.target.value)};
+    // function handlePasswordChange(e) {setPassword(e.target.value)};
+
+    const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation(); 
 
     function handleSubmit(e) {
         e.preventDefault(); // если это отсуствует, то после сабмита, компонент перезагружается и будет не видно запросов в dev-tools
                             // из-за этого убил кучу времени на отладку
-        props.onRegister(name, email, password);
+        props.onRegister(values.user, values.email, values.password);
     }
-
+    // const dis = isValid ? {} : {disabled: 'disabled'};
     return (
         <section className="register">
             <div className="entrance-form">
@@ -25,13 +28,13 @@ function Register(props) {
                 <h1 className="entrance-form__header">Добро пожаловать!</h1>
                 <form className="form" onSubmit={handleSubmit}>
                     <span className="form__input-title">Имя</span>
-                    <input type="text" className="form__input" onChange={handleNameChange} value={name} /> 
+                    <input name="user" type="text" className="form__input" onChange={handleChange} value={values.user} /> 
                     <span className="form__input-title">E-mail</span>
-                    <input type="email" className="form__input" onChange={handleEmailChange} value={email} />
+                    <input name="email" type="email" className="form__input" onChange={handleChange} value={values.email} />
                     <span className="form__input-title">Пароль</span>
-                    <input type="password" className="form__input" onChange={handlePasswordChange} value={password} />
+                    <input name="password" type="password" className="form__input" onChange={handleChange} value={values.password} />
                     <span className="error-span">Что-то пошло не так...</span> 
-                    <button className="form__button" type="submit">Зарегистрироваться</button>
+                    <button className="form__button" type="submit" {...!isValid ? {disabled: 'disabled'} : {}} >Зарегистрироваться</button>
                 </form>
                 <span className="is-registered">
                     Уже зарегистрированы? 
