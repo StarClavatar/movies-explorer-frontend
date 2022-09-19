dd6d2e4713abfaf9c19bc34bd4ca532497731c24
Kruglikov <yandexart@yandex.ru>
2022-09-19 17:41:41 +0300

    searching movies bugfix

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -23,49 +23,53 @@ function SearchForm(props) {
         []
     );
 
-    function searchTextChangeHandler(e) {setSearchText(e.target.value); if (savedMoviesMode) onSubmit(e)}
-    function shortsChangeHandler(e) {setShorts(e.target.checked)}
+    function searchTextChangeHandler(e) {setSearchText(e.target.value); if (savedMoviesMode) search(e.target.value, shorts)}
+    function shortsChangeHandler(e) {setShorts(e.target.checked); search(searchText, e.target.checked)}
 
     function onSubmit(e) {
         e.preventDefault();
+        search(searchText, shorts);
+    }
+
+    function search (searchText, shorts) {
         onSearch (searchText, shorts);
         if (!savedMoviesMode) localStorage.setItem('searchParams', JSON.stringify({searchText, shorts}))
     }
+    
     const req = savedMoviesMode ? {} : {required: 'required'};
     return (
-            <section className="search-form">
-                <div className='search-form__wrapper'>
-                    <form className="search-form__form" onSubmit={onSubmit}>
-                        <div className="search-form__film-container">
-                            <img src={searchIcon} alt="Значок поиска лупа" className="search-form__film-icon" />
-                            <input 
-                                className="search-form__film-input" 
-                                placeholder="Фильм" 
-                                type="text" 
-                                value={searchText}
-                                onChange={searchTextChangeHandler}
-                                onInvalid={e => e.target.setCustomValidity("Введите название фильма")}
-                                onInput={e => e.target.setCustomValidity("")}                                
-                                {...req}
-                            />
-                            <button className="button search-form__find-button" 
-                                type="submit"
-                                style={{backgroundImage: `url(${findIcon})`}} 
-                            />
-                        </div>
-                        <div className="form-filter">
-                            <input className="form-filter__checkbox" 
-                                type="checkbox" 
-                                checked={shorts}
-                                onChange={shortsChangeHandler}
-                            />
-                            <p className="form-filter__subsription">Короткометражки</p>
-                        </div>
-                        <span className='search-error'>чёто не так! Не годится!</span>
-                    </form>
-                </div>
+        <section className="search-form">
+            <div className='search-form__wrapper'>
+                <form className="search-form__form" onSubmit={onSubmit}>
+                    <div className="search-form__film-container">
+                        <img src={searchIcon} alt="Значок поиска лупа" className="search-form__film-icon" />
+                        <input 
+                            className="search-form__film-input" 
+                            placeholder="Фильм" 
+                            type="text" 
+                            value={searchText}
+                            onChange={searchTextChangeHandler}
+                            onInvalid={e => e.target.setCustomValidity("Введите название фильма")}
+                            onInput={e => e.target.setCustomValidity("")}                                
+                            {...req}
+                        />
+                        <button className="button search-form__find-button" 
+                            type="submit"
+                            style={{backgroundImage: `url(${findIcon})`}} 
+                        />
+                    </div>
+                    <div className="form-filter">
+                        <input className="form-filter__checkbox" 
+                            type="checkbox" 
+                            checked={shorts}
+                            onChange={shortsChangeHandler}
+                        />
+                        <p className="form-filter__subsription">Короткометражки</p>
+                    </div>
+                    <span className='search-error'>чёто не так! Не годится!</span>
+                </form>
+            </div>
         </section>
-        
     );    
 }
 
